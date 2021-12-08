d5cfc156a462f0b2a917b094fe865a0bfa6125ba
Liubov Sireneva <lsireneva@gmail.com>
2021-12-08 23:36:39 +0000

    Working discord bot

--- markov.py
+++ markov.py
@@ -1,66 +1,92 @@
 """A Markov chain generator that can tweet random messages."""
-
+import os
+import discord
 import sys
 from random import choice
 
 
-def open_and_read_file(filenames):
-    """Take list of files. Open them, read them, and return one long string."""
+client = discord.Client()
 
-    body = ''
-    for filename in filenames:
-        text_file = open(filename)
-        body = body + text_file.read()
-        text_file.close()
-
-    return body
+@client.event
+async def on_ready():
+    print(f'Successfully connected! Logged in as {client.user}.')
 
 
-def make_chains(text_string):
-    """Take input text as string; return dictionary of Markov chains."""
+@client.event
+async def on_message(message):
 
-    chains = {}
+    if message.author == client.user:
+        return
 
-    words = text_string.split()
-    for i in range(len(words) - 2):
-        key = (words[i], words[i + 1])
-        value = words[i + 2]
+    #if message.content.startswith('$hello'):
+    def open_and_read_file(filenames):
+        """Take list of files. Open them, read them, and return one long string."""
 
-        if key not in chains:
-            chains[key] = []
+        body = ''
+        for filename in filenames:
+            text_file = open(filename)
+            body = body + text_file.read()
+            text_file.close()
 
-        chains[key].append(value)
-
-    return chains
-
-
-def make_text(chains):
-    """Take dictionary of Markov chains; return random text."""
-
-    keys = list(chains.keys())
-    key = choice(keys)
-
-    words = [key[0], key[1]]
-    while key in chains:
-        # Keep looping until we have a key that isn't in the chains
-        # (which would mean it was the end of our original text).
-
-        # Note that for long texts (like a full book), this might mean
-        # it would run for a very long time.
-
-        word = choice(chains[key])
-        words.append(word)
-        key = (key[1], word)
-
-    return ' '.join(words)
+        return body
 
 
-# Get the filenames from the user through a command line prompt, ex:
-# python markov.py green-eggs.txt shakespeare.txt
-filenames = sys.argv[1:]
+    def make_chains(text_string):
+        """Take input text as string; return dictionary of Markov chains."""
 
-# Open the files and turn them into one long string
-text = open_and_read_file(filenames)
+        chains = {}
 
-# Get a Markov chain
-chains = make_chains(text)
+        words = text_string.split()
+        for i in range(len(words) - 2):
+            key = (words[i], words[i + 1])
+            value = words[i + 2]
+
+            if key not in chains:
+                chains[key] = []
+
+            chains[key].append(value)
+
+        return chains
+
+
+    def make_text(chains):
+        """Take dictionary of Markov chains; return random text."""
+
+        keys = list(chains.keys())
+        key = choice(keys)
+
+        words = [key[0], key[1]]
+        while key in chains:
+            # Keep looping until we have a key that isn't in the chains
+            # (which would mean it was the end of our original text).
+
+            # Note that for long texts (like a full book), this might mean
+            # it would run for a very long time.
+
+            word = choice(chains[key])
+            words.append(word)
+            key = (key[1], word)
+
+        return ' '.join(words)
+
+
+    # Get the filenames from the user through a command line prompt, ex:
+    # python markov.py green-eggs.txt shakespeare.txt
+    filenames = sys.argv[1:]
+
+    # Open the files and turn them into one long string
+    #filename = "green-eggs.txt"
+    text = open_and_read_file(filenames)
+
+    # Get a Markov chain
+    chains = make_chains(text)
+    print("text:",text)
+    print("chains:", chains)
+    #await message.channel.send('Hello from Luba Sandy bot')
+    await message.channel.send(make_text(chains))
+    
+
+
+client.run(os.environ['DISCORD_TOKEN'])
+
+
